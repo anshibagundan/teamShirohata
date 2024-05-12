39204838e4588c14cf4023fe5c3d42094a0e5af6
zono0013 <143684883+zono0013@users.noreply.github.com>
2024-05-12 17:08:49 +0900

    いったん削除

--- vr_museum/vr_museum/src/main/java/com/github/anshibagundan/vr_museum/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.anshibagundan.vr_museum.repository;
-
-import com.github.anshibagundan.vr_museum.entity.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
