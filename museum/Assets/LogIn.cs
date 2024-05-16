914610493bc98339e9533a8417a1f6010e4f5da7
stk1201 <satokichi121@gmail.com>
2024-05-16 15:32:12 +0900

    ログイン（ハッシュ化失敗）

--- museum/Assets/LogIn.cs
+++ museum/Assets/LogIn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
 using TMPro;
+using Newtonsoft.Json;
+
 
 public class LogIn : MonoBehaviour
 {
@@ -31,7 +34,8 @@ public class LogIn : MonoBehaviour
 
         //Unity用のユーザ名をテキストフィールドから代入する。
         userFromU = inputUserName.text;
-        hashedPasswordFromU = Hash(inputPassword.text);
+        string salt;
+        hashedPasswordFromU = Hash(inputPassword.text, out salt);
 
         //データベースからuserFromUと一致するuserを探しそのuserのパスワードをpasswordFromDBに代入する。
         string url = "https://vr-museum-6034ae04d19d.herokuapp.com/api/user_model/";
@@ -67,28 +71,33 @@ public class LogIn : MonoBehaviour
     }
 
     //ハッシュ化
-    private string Hash(string password){
-        using(SHA256 sha256 = SHA256.Create()){
-            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            StringBuilder builder = new StringBuilder();
-            for(int i = 0; i < bytes.Length; i++){
-                builder.Append(bytes[i].ToString("x2"));//連結して16進数に変換
+    private string Hash(string password, out string salt){
+        // ソルトを生成
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            byte[] saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            salt = Convert.ToBase64String(saltBytes);
+
+            // PBKDF2を使ってパスワードをハッシュ化
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 720000, HashAlgorithmName.SHA256))
+            {
+                byte[] hash = pbkdf2.GetBytes(32); // 256ビットのハッシュを生成
+                return Convert.ToBase64String(hash);
             }
-            return builder.ToString();
         }
     }
 
     //DBからデータ取得する
-    private async Task<List<MyData>> FetchData(string url){
+    async Task<List<MyData>> FetchData(string url){
     
         using (HttpClient client = new HttpClient()){//HTTPリクエストを送信し、受信する
             HttpResponseMessage response = await client.GetAsync(url);//レスポンス結果
 
             if(response.IsSuccessStatusCode){//レスポンスが正常に取得できた時、データを取得する
                 string responseData = await response.Content.ReadAsStringAsync();
-                string jsonData = responseData.TrimStart('[').TrimEnd(']');
-                Debug.Log(jsonData);
-                return JsonUtility.FromJson<List<MyData>>(jsonData);
+                //string jsonData = responseData.TrimStart('[').TrimEnd(']');
+                return JsonConvert.DeserializeObject<List<MyData>>(responseData);
             }
             else{//エラー処理
                 Debug.LogError("Error: " + response.StatusCode);
