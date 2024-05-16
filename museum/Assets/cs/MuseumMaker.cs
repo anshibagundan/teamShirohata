9f78a22a4b07bcbac7ae347bf8416cfa5d1bddc3
bearl <125728897+bearl27@users.noreply.github.com>
2024-05-16 16:52:54 +0900

    歩くのも，写真の配置も完成

--- museum/Assets/cs/MuseumMaker.cs
+++ museum/Assets/cs/MuseumMaker.cs
@@ -1,10 +1,9 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MuseumMaker : MonoBehaviour
 {
-    public List<string> v = new List<string> { "S", "S", "S", "S", "S", "R", "R", "R", "R1", "R1", "S", "S", "R2" };
+    private List<string> v = new List<string> { "S", "S", "S", "S", "S", "R", "R", "R", "R", "R", "R",　"R", "R", "R", "R1", "R1", "S", "S", "R2" };
         
     public GameObject streetPrefab; 
     public GameObject roomPrefab; 
@@ -14,24 +13,24 @@ public class MuseumMaker : MonoBehaviour
     //写真関連
     public GameObject exhibitPrefab;  // スペルミス修正
     
-    private Vector3 exhibitStart = new Vector3(10, 5, -15); // 開始位置
+    private Vector3 exhibitStart = new Vector3(8, 10, -15); // 開始位置
     private Vector3 exhibitOffset = new Vector3(0, 0, 10); // 各インスタンスの位置オフセット
-    private Vector3 leftsidePosition = new Vector3(-10, 5, -12);
+    private Vector3 leftsidePosition = new Vector3(-10, 10, -12);
     private Vector3 leftsideRotation = new Vector3(0, 180, 0);
     
     public static int streetNum = 0;
 
     private List<Vector3> roomPhotoPos = new List<Vector3>
     {
-        new Vector3(-60, 10 , 0),//1
-        new Vector3(-40, 10 , -20),//2
-        new Vector3(-40, 10 , 20),//3
-        new Vector3(-60, 10 , 10),//4
-        new Vector3(-60, 10 , -10),//5
-        new Vector3(-30, 10 , -20),//6
-        new Vector3(-30, 10 , 20),//7
-        new Vector3(-50, 10 , -20),//8
-        new Vector3(-50, 10 , 20)//9
+        new Vector3(-58, 10 , 0),//1
+        new Vector3(-40, 10 , -18),//2
+        new Vector3(-40, 10 , 18),//3
+        new Vector3(-58, 10 , 10),//4
+        new Vector3(-58, 10 , -10),//5
+        new Vector3(-30, 10 , -18),//6
+        new Vector3(-30, 10 , 18),//7
+        new Vector3(-50, 10 , -18),//8
+        new Vector3(-50, 10 , 18)//9
     };
     
     private List<Vector3> roomPhotoRote = new List<Vector3>
